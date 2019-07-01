5ef6df4f9951f10b23e54fc85c9aaa49faab92e2
Gustavo Henrique F. Junqueira <gustavohenriquejunqueira@hotmail.com>
2019-07-01 13:48:51 -0300

    javaScript
    Demonstrando como adicionar comentarios em um js

--- javaScript/meu_javascript.js
+++ javaScript/meu_javascript.js
@@ -1,1 +1,12 @@
 alert("Este e um teste de inclusao de js a partir de um arquivo externo");
+
+
+//Comentario de uma unica linha
+
+/*
+Bloco de comentarios
+utilizado 
+para mais 
+de uma linha
+
+*/
